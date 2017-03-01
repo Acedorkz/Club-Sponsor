7d7abf3fd3430e785b5c9153c292cfaf4bb00d0f
Moersity <228609157@qq.com>
2017-03-01 16:49:47 +0800

    修改模型

--- Social/settings.py
+++ Social/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT
+import easyfundraising
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,10 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'easyfundraising',
     'ckeditor',
     'rest_framework',
-    'users',
+    'easyfundraising',
+
 ]
 
 
@@ -120,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL='users.User'
+
 
 
 # Static files (CSS, JavaScript, Images)
--- easyfundraising/models.py
+++ easyfundraising/models.py
@@ -8,9 +8,12 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    
+    username = models.CharField('用户名',max_length=30)
+    password = models.CharField('密码',max_length=30)
+    email = models.EmailField('邮箱')
     def __unicode__(self):
         return self.username
+   
 class Item(models.Model):
     author = models.ForeignKey(User,related_name='items',default=0)
     title = models.CharField(max_length=30)
@@ -25,7 +28,7 @@ class UserDetail(models.Model):
     social_name = models.CharField("社团名",max_length=50)
     phone_num = models.CharField(max_length=11,verbose_name='电话号码')
     def __unicode__(self):
-        return self.user.username
-    
+        return self.social_name
+
     
     
